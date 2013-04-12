142ca70625435bb1cd827cc0f6bba77452bd73b3
3r1k <ekemperman@ask-cs.com>
2013-04-12 13:10:16 +0200

    Allow affectbutton to be included directly in xml, and rely on background attribute to toggle transparency

---
+++ src/com/askcs/android/util/Constants.java
@@ -0,0 +1,14 @@
+package com.askcs.android.util;
+
+
+public class Constants {
+	
+	private Constants() {
+		// everything is static, prevent instantiation
+	}
+	
+	
+	static public final String ANDROID_NS = "http://schemas.android.com/apk/res/android";
+	static public final String ASK_NS = "http://schemas.android.com/apk/res/com.askcs.android.timeout";
+	
+}

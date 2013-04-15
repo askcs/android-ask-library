252ab850babbd09eed7d3300e658280083512e7b
izwanink@ask-cs.com <izwanink@ask-cs.com>
2013-04-15 10:42:54 +0200

    Added start update service on boot support

---
+++ src/com/askcs/android/appservices/BootReceiver.java
@@ -0,0 +1,44 @@
+package com.askcs.android.appservices;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.askcs.android.util.Prefs;
+
+public class BootReceiver extends BroadcastReceiver {
+	
+	@Override
+	public void onReceive(Context context, Intent receivedIntent) {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		if (!prefs.getString(Prefs.EMAIL, "").equals("")
+				&& !prefs.getString(Prefs.PASSWORD, "").equals("")) {
+
+			Intent intent = new Intent(context, SyncAlarmReceiver.class);
+
+			// Check if already scheduled
+			if (PendingIntent.getBroadcast(context, 0, intent,
+					PendingIntent.FLAG_NO_CREATE) == null) {
+
+				AlarmManager mgr = (AlarmManager) context
+						.getSystemService(Context.ALARM_SERVICE);
+				PendingIntent pendingIntent = PendingIntent.getBroadcast(
+						context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+				mgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+						SystemClock.elapsedRealtime(), prefs.getLong(
+								"updateFrequency",
+								AlarmManager.INTERVAL_FIFTEEN_MINUTES),
+						pendingIntent);
+
+			}
+
+		}
+	}
+}

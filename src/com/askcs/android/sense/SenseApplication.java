2d558309922739de97dbf0de3e28a9074f3307da
3r1k <ekemperman@ask-cs.com>
2013-04-18 12:17:39 +0200

    Move more into general SenseApplication

--- src/com/askcs/android/sense/SenseApplication.java
+++ src/com/askcs/android/sense/SenseApplication.java
@@ -1,17 +1,73 @@
 package com.askcs.android.sense;
 
 import nl.sense_os.platform.SensePlatform;
+import nl.sense_os.service.ServiceStateHelper;
 import android.app.Application;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
 
-public class SenseApplication extends Application {
+import com.askcs.android.util.Prefs;
+
+public class SenseApplication extends Application implements ServiceConnection  {
 
 	protected SensePlatform mSensePlatform;
 
 	public SenseApplication() {
-
+		mSensePlatform = new SensePlatform(this, this);
 	}
 
 	public SensePlatform getSensePlatform() {
 		return mSensePlatform;
+	}
+	
+	public void setSensePlatform(SensePlatform sensePlatform) {
+		this.mSensePlatform = sensePlatform;
+	}
+	
+	@Override
+	public void onServiceConnected(ComponentName name, IBinder service) {
+
+		// check the sense service status
+		new Thread() {
+
+			@Override
+			public void run() {
+				if (isLoggedIn()) {
+					startSense();
+				}
+			}
+		}.start();
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName name) {
+		// nothing to do
+	}
+	
+
+	/**
+	 * @return true if there is a session ID
+	 */
+	public boolean isLoggedIn() {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		return prefs.getString(Prefs.SESSION_ID, null) != null;
+	}
+
+
+
+	/**
+	 * Starts the Sense service (if it is not running yet)
+	 */
+	private void startSense() {
+		ServiceStateHelper ssh = ServiceStateHelper
+				.getInstance(getApplicationContext());
+		if (!ssh.isStarted()) {
+			mSensePlatform.getService().toggleMain(true);
+			
+		}
 	}
 }

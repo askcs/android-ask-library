68410ae419c27595e1aef18987081df34c7f8c65
3r1k <ekemperman@ask-cs.com>
2013-04-25 10:07:53 +0200

    pushed again by cengiz

---
+++ src/com/askcs/android/json/JsonMap.java
@@ -0,0 +1,32 @@
+package com.askcs.android.json;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class JsonMap
+extends LinkedHashMap<String, String> {
+	
+	static private final String TAG = "JsonMap";
+	
+	static public TreeNode parse( String string ) throws JsonParseException, IOException {
+		JsonFactory factory = new JsonFactory();
+		JsonParser parser = factory.createParser( string );
+		ObjectMapper mapper = new ObjectMapper();
+		TreeNode tree = mapper.readTree( parser );
+		return tree;
+	}
+	
+	private JsonMap() {
+	}
+	
+	
+	
+	
+}
